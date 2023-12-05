7b3d9c4083b68f5a6dae8f533a62f11890e168d0
Govind-Sneha01 <govinda.aadhi@gmail.com>
2023-12-05 10:05:05 +0530

    Added:Calculator program

---
+++ calculator.js
@@ -0,0 +1,12 @@
+import Calculator from './calculator';
+
+function App() {
+    return (
+        <div className="App">
+            <h1 style={{textAlign:'center'}}>
+              Calculator
+            </h1>
+            <Calculator />
+        </div>
+    )
+}
